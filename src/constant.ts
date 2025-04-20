e61b2e96a06127d57ca0018ebdaa64834a78f08f
karnan <karnan.se@gmail.com>
2025-04-20 16:00:13 +0530

    goiing to jhost

--- src/constant.ts
+++ src/constant.ts
@@ -1,5 +1,5 @@
 let environment = (import.meta.env.VITE_ENVIRONMENT) ?? "production"
 
 export const configKeys = {
-    BaseUrl : environment == "development" ? "http://localhost:3000" : ""
+    BaseUrl : environment == "development" ? "http://localhost:3000" : "https://urlshort.vingle.shop"
 }
